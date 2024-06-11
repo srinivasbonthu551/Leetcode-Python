8c9245f9ab1ed9e2efe9efc2290e9ab635e2915c
Bonthu Srinivasu <130753139+srinivasbonthu551@users.noreply.github.com>
2024-06-11 12:03:18 +0530

    Add files via upload

---
+++ TWO_SUM.py
@@ -0,0 +1,11 @@
+def two_sum(nums, target):
+    num_map = {}
+    for i , num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return None
+nums = [2, 7,11,15]
+target = 26
+print(two_sum(nums,target))
