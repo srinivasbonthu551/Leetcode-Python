e54f4d87bb44980018da5968dd66f37ecc55203e
Bonthu Srinivasu <130753139+srinivasbonthu551@users.noreply.github.com>
2024-07-29 17:32:34 +0530

    Update Fibonacci_Series.py

--- Fibonacci_Series.py
+++ Fibonacci_Series.py
@@ -1,12 +1,14 @@
-class solution:
-    def fibonacci(self, n:int)->int:
+class Solution:
+    def fibonacci(self, n: int) -> list:
         if n == 0:
-            return 0
+            return [0]
         elif n == 1:
-            return 1
-        ans = [0,1]
-        for i in range(2 , n+1):
-            ans.append(ans[i-1] + ans[i-2])
+            return [0, 1]
+        ans = [0, 1]
+        for i in range(2, n + 1):
+            ans.append(ans[i - 1] + ans[i - 2])
         return ans
-sol = solution()
-print(sol.fibonacci(6))
+
+sol = Solution()
+print(sol.fibonacci(6)) 
+
